9b2dccd9e00baf1b83b160f29c0a9176ecf1d4f8
Arthrai619 <167521859+Arthrai619@users.noreply.github.com>
2025-09-22 23:13:54 +0530

    Add files via upload

---
+++ Backend/Middleware/uploadMiddleware.js
@@ -0,0 +1,30 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 10000000, // 10 MB
+  },
+  fileFilter: (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    }
+    cb('Error: File upload only supports the following filetypes - ' + filetypes);
+  },
+});
+
+module.exports = upload;
